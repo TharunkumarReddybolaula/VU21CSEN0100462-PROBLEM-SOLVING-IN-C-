9cf1f3fcbd2acc23bdd8d9032eeca0f515b259e2
TharunkumarReddybolaula <96126529+TharunkumarReddybolaula@users.noreply.github.com>
2021-12-14 23:34:38 +0530

    Create temparature.c

---
+++ temparature.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float c,f;
+    printf("to find out the total temperature in climate");
+    scanf("%f",&f);
+    c=0.5*(f-32);
+    printf("%f",c);
+
+    return 0;
+}
